6a216e86b87f4fb494e3c9dc95453721e5294690
engineerDavid <33636080+engineerDavid@users.noreply.github.com>
2021-05-16 22:18:21 -0500

    testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-
+//test
 function createEntry(emojiTerm){
   return( <Entry 
   key={emojiTerm.id}
